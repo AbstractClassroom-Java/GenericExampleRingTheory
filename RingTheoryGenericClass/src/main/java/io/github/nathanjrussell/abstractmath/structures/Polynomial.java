39891fe6b8a25cb91c8f57bbd1c15aa9999f6792
Nathan Russell <nathan@blackpathway.com>
2025-11-03 09:54:11 -0500

    started the polynomial class

--- RingTheoryGenericClass/src/main/java/io/github/nathanjrussell/abstractmath/structures/Polynomial.java
+++ RingTheoryGenericClass/src/main/java/io/github/nathanjrussell/abstractmath/structures/Polynomial.java
@@ -2,7 +2,45 @@ package io.github.nathanjrussell.abstractmath.structures;
 
 import io.github.nathanjrussell.abstractmath.ring.IRingElement;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Polynomial<T extends IRingElement<T>> implements IRingElement<Polynomial<T>> {
     int deg;
+    List<T> coefficients;
+
+
+    Polynomial(int deg) {
+        this.deg = deg;
+        validateDegree(deg);
+
+        this.coefficients = new ArrayList<>(deg + 1);
+    }
+
+    public void setCoefficient(int degree, T coefficient) {
+        validateDegree(degree);
+        this.coefficients.set(degree, coefficient);
+    }
+
+    public T getCoefficient(int degree) {
+        validateDegree(degree);
+        return this.coefficients.get(degree);
+    }
+
+    private void validateDegree(int degree) {
+        if (degree < 0) {
+            throw new IllegalArgumentException("Degree must be non-negative.");
+        }
+        if (degree > this.deg) {
+            throw new IllegalArgumentException("Degree exceeds polynomial degree.");
+        }
+    }
 
 }
+
+
+
+
+
+
+
